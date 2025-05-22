70015d3ac8c8184b6741d370d3dd03bc4a506aee
emilia-nodz <extra.maslo.game@gmail.com>
2025-05-22 21:30:16 +0200

    DAO ogłoszenia

--- projekt/src/main/java/com/demo/dao/NoticeDAO.java
+++ projekt/src/main/java/com/demo/dao/NoticeDAO.java
@@ -1,6 +1,13 @@
 package com.demo.dao;
 
 import com.demo.models.Notice;
+import com.demo.models.User;
 
-public interface NoticeDAO extends AbstractDAO{
+import java.util.Date;
+import java.util.List;
+
+public interface NoticeDAO extends AbstractDAO<Notice> {
+    List<Notice> findByTitle(String title);
+    List<Notice> findByPublishedDate(Date publishedDate);
+    List<Notice> findByAuthor(User author);
 }
---
+++ projekt/src/main/java/com/demo/dao/NoticeDAOImpl.java
@@ -0,0 +1,54 @@
+package com.demo.dao;
+
+import com.demo.models.Notice;
+import com.demo.models.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import jakarta.ejb.Stateless;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+
+@Stateless
+public class NoticeDAOImpl extends AbstractDAOImpl<Notice> implements NoticeDAO{
+    private static final Logger logger = LogManager.getLogger(NoticeDAOImpl.class);
+
+    @Override
+    public List<Notice> findByTitle(String title) {
+        logger.debug("Szukanie ogłoszeń o nazwie: {}", title);
+        return em.createQuery(
+                        "SELECT n FROM Notice n WHERE n.title = :title", Notice.class)
+                .setParameter("title", title)
+                .getResultList();
+    }
+
+    @Override
+    public List<Notice> findByPublishedDate(Date publishedDate){
+        LocalDate localDate = publishedDate.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+
+        logger.debug("Szukanie ogłoszeń stworzonych dnia: {}", localDate);
+
+        Date startOfDay = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        Date nextDay = Date.from(localDate.plusDays(1).atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        return em.createQuery(
+                        "SELECT n FROM Notice n WHERE n.publishDate >= :startOfDay AND n.publishDate < :nextDay", Notice.class)
+                .setParameter("startOfDay", startOfDay)
+                .setParameter("nextDay", nextDay)
+                .getResultList();
+    }
+
+    @Override
+    public List<Notice> findByAuthor(User author) {
+        logger.debug("Szukanie ogłoszeń po autorze: {} {}", author.getName(), author.getSurname());
+        return em.createQuery(
+                        "SELECT n FROM Notice n WHERE n.author = :author", Notice.class)
+                .setParameter("author", author)
+                .getResultList();
+    }
+}
